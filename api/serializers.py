3388fa1113d5c8af9113dba84bc96e1cfaab0741
James Andrews <jbandrew14@gmail.com>
2022-01-14 20:55:47 -0500

    added endpoint to for user game detail and user library; game detail retrieve works, need to sort out updatel; user library needs tweeking

---
+++ api/serializers.py
@@ -0,0 +1,41 @@
+from rest_framework import serializers
+from .models import Game, CustomUser
+
+
+class GameListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Game
+        fields = (
+            'pk',
+            'bgg',
+            'title',
+            'pub_year',
+            'image',
+        )
+
+
+class UserNestedSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = (
+            'pk',
+            'username',
+            'avatar',
+        )
+
+
+class GameDetailSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Game
+        fields = (
+            'title',
+            'bgg',
+            'pub_year',
+            'description',
+            'min_players',
+            'max_players',
+            'image',
+            'playtime',
+            'player_age',
+        )
