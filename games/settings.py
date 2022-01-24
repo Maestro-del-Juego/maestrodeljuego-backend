11636ad0f264f10d00163ecf66e34055a324cfd0
James Andrews <jbandrew14@gmail.com>
2022-01-24 16:50:37 -0500

    added email settings

--- games/settings.py
+++ games/settings.py
@@ -167,3 +167,11 @@ DJOSER = {
 }
 
 AUTH_USER_MODEL = 'api.CustomUser'
+
+# email settings
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = env('EMAIL_HOST')
+EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASS')
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
