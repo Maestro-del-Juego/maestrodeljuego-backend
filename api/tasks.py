a4c5395bedbb85c123611aaaf854dba1cb363aaa
James Andrews <jbandrew14@gmail.com>
2022-01-29 19:28:33 -0500

    set up task to send test email

--- api/tasks.py
+++ api/tasks.py
@@ -1,5 +1,17 @@
 from datetime import datetime, date
 from celery import Celery
+from django.core.mail import send_mail
+from games import settings
 
-app = Celery()
+app = Celery('games', broker='redis://localhost')
 
+@app.task
+def test_email(dt):
+    message = f"The datetime is {dt}."
+    email_list = ['jbandrew14@gmail.com']
+    send_mail(
+        subject='testing',
+        message=message,
+        from_email=settings.EMAIL_HOST_USER,
+        recipient_list=email_list
+    )
