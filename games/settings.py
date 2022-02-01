4326e21143001a9115a6a16e0a730ec4d5082841
James Andrews <jbandrew14@gmail.com>
2022-02-01 13:10:17 -0500

    switched email backend

--- games/settings.py
+++ games/settings.py
@@ -170,8 +170,8 @@ AUTH_USER_MODEL = 'api.CustomUser'
 
 
 # email settings
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = env('EMAIL_HOST')
 EMAIL_HOST_USER = env('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASS')
